a2f5dbf4a1306ecf96928f1df24eb1685f20e186
Ales4135 <aliejusnoob@gmail.com>
2023-05-14 19:08:04 +0300

    builded

--- Zaidimas/Assets/Scripts/PlayerController.cs
+++ Zaidimas/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using UnityEditorInternal;
+//using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
